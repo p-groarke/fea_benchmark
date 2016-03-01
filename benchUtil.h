bbb28386d2fbfb8ca8125093d5f695100a8e1cba
pgroarke <pgroarke>
2016-03-01 18:46:20 -0500

    Utils and benchmarks.

---
+++ benchUtil.h
@@ -0,0 +1,26 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <chrono>
+
+namespace Bench {
+	std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
+	std::chrono::duration<double> elapsedTime;
+
+	void title(const std::string& title) {
+		std::cout << std::endl << std::string(title.size(), '-') << std::endl;
+		std::cout << title << std::endl;
+		std::cout << std::string(title.size(), '-') << std::endl;
+	}
+
+	void start(const std::string& message = "") {
+		std::cout << message << std::endl;
+		startTime = std::chrono::system_clock::now();
+	}
+
+	void end(const std::string& message = "") {
+		endTime = std::chrono::system_clock::now();
+		elapsedTime = endTime - startTime;
+		std::cout << message << " took: " << setiosflags(std::ios::fixed) << elapsedTime.count() << "s" << std::endl;
+	}
+};
---
+++ const ref vs copy/main.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+
+#include "benchUtil.h"
+
+double constFib(const double& n) {
+	if (n < 2)
+		return n;
+	return constFib(n - 1) + constFib(n - 2);
+}
+
+double copyFib(double n) {
+	if (n < 2)
+		return n;
+	return copyFib(n - 1) + copyFib(n - 2);
+}
+
+int main(int argc, char** argv) {
+	Bench::title("const reference VS. copy -- using doubles");
+
+	Bench::start();
+	std::cout << "Fib = " << constFib(42) << std::endl;
+	Bench::end("const double&");
+
+	Bench::start();
+	std::cout << "Fib = " << copyFib(42) << std::endl;
+	Bench::end("copy");
+
+	return 0;
+}
+
+
+// const int qty = 10000000;
+//
+// void constFunc(const int& i) {
+// 	int bleh = i + i;
+// }
+//
+// void copyFunc(int i) {
+// 	int bleh = i + i;
+// }
+//
+//int main(int argc, char** argv) {
+//	std::vector<int> vec(qty);
+//	std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
+//	std::chrono::duration<double> elapsedTime;
+//
+//	std::fill(vec.begin(), vec.end(), 1);
+//
+//	// const ref test
+//	startTime = std::chrono::system_clock::now();
+//	for (int i = 0; i < qty; ++i) {
+//		constFunc(vec[i]);
+//	}
+//	endTime = std::chrono::system_clock::now();
+//	elapsedTime = endTime - startTime;
+//	std::cout << "Const reference function took: " << elapsedTime.count() << "s" << std::endl;
+//
+//	// copy test
+//	startTime = std::chrono::system_clock::now();
+//	for (int i = 0; i < qty; ++i) {
+//		copyFunc(vec[i]);
+//	}
+//	endTime = std::chrono::system_clock::now();
+//	elapsedTime = endTime - startTime;
+//	std::cout << "Copy funtion took: " << elapsedTime.count() << "s" << std::endl;
+//
+//	return 0;
+//}
